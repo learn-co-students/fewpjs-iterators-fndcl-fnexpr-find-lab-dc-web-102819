2b6fecfed0ac6a5543086a8dc466e52fe85303ff
Kyle <kylepcole@gmail.com>
2019-12-09 16:50:10 -0500

    lab complete

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+	let win = array.find(year => year.result === "W");
+	if (win) {
+		return win.year
+	}
+}
