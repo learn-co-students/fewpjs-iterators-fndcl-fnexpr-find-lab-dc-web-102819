a16792298a05082ac765873a9ded50b87d1a0678
mattbechtel1 <bechma02@gmail.com>
2019-12-09 16:52:58 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+// function returnRecord(record) {
+//   console.log(record[0].year)
+// }
+
+
+function superbowlWin(sbList) {
+  let win = sbList.find(function(rec) {
+      return rec.result === "W"
+    }
+  )
+  if (win === undefined) 
+    { return undefined }
+  else 
+    { return win.year }  
+}
